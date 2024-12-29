351de8d9eea228e41c5fb3db4e43e45a0a178820
Krustycoin0 <133280489+Krustycoin0@users.noreply.github.com>
2024-12-29 10:16:40 +0100

    Create TokenPromotionApplication.java

---
+++ TokenPromotionApplication.java
@@ -0,0 +1,12 @@
+package com.tokenpromotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenPromotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenPromotionApplication.class, args);
+	}
+}
